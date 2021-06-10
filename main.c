f01474eeeead9a3df22cc86fd573327136f31b45
dzrynn <85537754+dzrynn@users.noreply.github.com>
2021-06-10 17:18:32 +0800

    added head and tail

--- main.c
+++ main.c
@@ -3,27 +3,30 @@
 /* #include "Board_LED.h" */
 
 #define CBUFFER_SIZE 8
+#define DATA_SIZE 9
 
 int CBUFFER[CBUFFER_SIZE];
-int data[9] = {1,2,3,4,5,6,7,8,9};
-int output [9];
-int cbufferIndex = 0;
+int data[DATA_SIZE] = {1,2,3,4,5,6,7,8,9};
+int output [DATA_SIZE];
+int cbufferHead = 0;
+int cbufferTail = 0;
 int i = 0;
 int j = 0;
-/* int readIndex = 0;
-int writeIndex = 0; */
 
 void producer_thread (
 	/*---
 		wait or get event
 		check if empty???
-		head & tail
 		----*/
 	osSemaphoreWait(doneConsume, osWaitForever);
 	osMutexWait(buffMutex, osWaitForever);
-	CBUFFER[cbufferIndex] = data[i];
-	cbufferIndex++;
+	CBUFFER[cbufferTail] = data[i];
 	i++;
+    if cbufferTail = 7 
+    (
+        cbufferTail = 0;
+    )
+    else cbufferTail++;
 	osMutexRelease(buffMutex);
 	osSemaphoreRelease(doneProduce);
 )
@@ -31,9 +34,14 @@ void producer_thread (
 void consumer_thread (
 	osSemaphoreWait(doneProduce, osWaitForever);
 	osMutexWait(buffMutex, osWaitForever);
-	output[j] = CBUFFER[cbufferIndex];
+	output[j] = CBUFFER[cbufferHead];
 	j++;
-	cbufferIndex--;
+    /* how to remove value from buffer?? cannot delete element from array*/
+    if cbufferHead = 7
+    (
+        cbufferHead = 0;
+    )
+	else cbufferHead++;
 	osMutexRelease(buffMutex);
 	osSemaphoreRelease(doneConsume);
 )
